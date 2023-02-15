53fd235987137230debe3d95ce925a0bc5e1e355
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-02-15 05:21:37 -0500

    Use session variable instead of global
    use session variable
    * call python manage.py migrate after creating session variable

--- django1/tasks/views.py
+++ django1/tasks/views.py
@@ -3,15 +3,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-tasks = []
-
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
+
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 
@@ -20,11 +21,11 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
-                "form":form
+                "form": form
             })
 
     return render(request, "tasks/add.html", {
